56442a7c55b3a561e3d171d1585f567319dd5fe9
jasonbakker084 <jasonbakker084@gmail.com>
2019-09-11 14:07:36 +0200

    first commit

---
+++ src/main/java/com/novi/fahrenheit/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.novi.fahrenheit;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author jasonbakker
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    float temperature;
+    Scanner in = new Scanner(System.in);
+ 
+    System.out.println("Enter temperature in Fahrenheit");
+    temperature = in.nextInt();
+ 
+    temperature = ((temperature - 32)*5)/9;
+ 
+    System.out.println("temperature in Celsius = " + temperature);
+        System.out.println("jason");
+        System.out.println("bakker");
+        System.out.println("test1");
+        System.out.println("test2");
+        System.out.println("test3");
+  }
+}
